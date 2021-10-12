7d0888e14bdb6ecfe80c385a4c2e584e82b60993
Manny Verma <mverma7845@gmail.com>
2021-10-12 17:42:55 -0600

    pushing

---
+++ js/main.js
@@ -0,0 +1,38 @@
+console.clear();
+
+const texts = ["Front End Web Developer", "Computer Engineer", "Computer Enthusiast"]
+let index = 0;
+let count = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0,++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type,400);
+}());
+
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0,++index);
+
+    document.querySelector(".typing2").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type,400);
+}());
