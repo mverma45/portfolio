ca3dd3d34381e77170a5b59c1aa53b4bb98a7239
Manny Verma <mverma7845@gmail.com>
2021-11-10 21:38:21 -0700

    adding typewriter

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// console.clear();
+console.clear();
 
-// const texts = ["Front End Web Developer", "Computer Engineer", "Computer Enthusiast"]
-// let index = 0;
-// let count = 0;
-// let currentText = "";
-// let letter = "";
+const texts = ["Front End Web Developer", "Computer Engineer", "Computer Enthusiast"]
+let index = 0;
+let count = 0;
+let currentText = "";
+let letter = "";
 
-// (function type() {
-//     if (count === texts.length) {
-//         count = 0;
-//     }
-//     currentText = texts[count];
-//     letter = currentText.slice(0,++index);
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0,++index);
 
-//     document.querySelector(".typing").textContent = letter;
-//     if (letter.length === currentText.length) {
-//         count++;
-//         index = 0;
-//     }
-//     setTimeout(type,500);
-// }());
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type,500);
+}());
 
 
-// (function type() {
-//     if (count === texts.length) {
-//         count = 0;
-//     }
-//     currentText = texts[count];
-//     letter = currentText.slice(0,++index);
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0,++index);
 
-// }());
+}());
